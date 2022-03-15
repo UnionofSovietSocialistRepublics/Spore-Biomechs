69bb534f91cfacff02135cf2da017bda52d2450c
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2022-03-15 17:21:59 +0700

    Bane bane boom

--- scripts/unit.js
+++ scripts/unit.js
@@ -1,7 +1,14 @@
 const Drone = extendContent(UnitType, "Drone", {});
+const Baneling = extendContent(UnitType,  "Baneling", {});
+
 Drone.constructor = () => extend(UnitEntity, {});
 Drone.defaultController = () => extend(MinerAI, {});
 
+Baneling.constructor = () => extend(UnitEntity, {});
+Baneling.defaultController = () => extend(SuicideAI, {});
+
+
 module.exports = {
-  Drone: Drone
+  Drone: Drone,
+  Baneling: Baneling
 }
