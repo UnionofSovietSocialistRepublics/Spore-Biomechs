ce56579a28253ce07d648b3b792e80ebbd1f32c3
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2022-12-28 17:34:34 +0700

    sex

--- scripts/unit.js
+++ scripts/unit.js
@@ -1,11 +1,15 @@
 const Drone = extendContent(UnitType, "Drone", {});
 const Baneling = extendContent(UnitType,  "Baneling", {});
+const Interceptor = extendContent(UnitType,  "Interceptor", {});
 
 Drone.constructor = () => extend(UnitEntity, {});
 Drone.defaultController = () => extend(MinerAI, {});
 
 Baneling.constructor = () => extend(UnitEntity, {});
 Baneling.defaultController = () => extend(SuicideAI, {});
+
+Interceptor.constructor = () => extend(UnitEntity, {});
+Interceptor.defaultController = () => extend(FlyingAI, {});
 
 
 module.exports = {
