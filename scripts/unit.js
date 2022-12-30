6627aa300b5b2bdadb8c56c0211e8c06967e755c
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2022-12-30 05:16:34 +0000

    blame hydro, he split the mod in two

--- scripts/unit.js
+++ scripts/unit.js
@@ -1,6 +1,5 @@
-const Drone = extendContent(UnitType, "Drone", {});
-const Baneling = extendContent(UnitType,  "Baneling", {});
-const Interceptor = extendContent(UnitType,  "Interceptor", {});
+const Drone = extend(UnitType, "Drone", {});
+const Baneling = extend(UnitType,  "Baneling", {});
 
 Drone.constructor = () => extend(UnitEntity, {});
 Drone.defaultController = () => extend(MinerAI, {});
@@ -8,12 +7,8 @@ Drone.defaultController = () => extend(MinerAI, {});
 Baneling.constructor = () => extend(UnitEntity, {});
 Baneling.defaultController = () => extend(SuicideAI, {});
 
-Interceptor.constructor = () => extend(UnitEntity, {});
-Interceptor.defaultController = () => extend(FlyingAI, {});
-
 
 module.exports = {
   Drone: Drone,
-  Baneling: Baneling,
-  Interceptor: Interceptor
+  Baneling: Baneling
 }
