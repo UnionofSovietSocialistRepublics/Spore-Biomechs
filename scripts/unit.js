805d893ff01fdc34056aa25ac3038d847644a57a
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2023-01-07 08:50:02 +0000

    oi oi oi beta is here

--- scripts/unit.js
+++ scripts/unit.js
@@ -1,6 +1,7 @@
 const Drone = extend(UnitType, "Drone", {});
 const Baneling = extend(UnitType,  "Baneling", {});
-
+//this is relic of the past now
+//read it if you want
 Drone.constructor = () => extend(UnitEntity, {});
 Drone.defaultController = () => extend(MinerAI, {});
 
