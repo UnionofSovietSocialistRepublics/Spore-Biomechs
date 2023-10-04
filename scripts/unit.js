a68716511a24bc8dfddc67487cbee78cf02a2ceb
Melodeiro <d1b@bk.ru>
2023-10-04 17:24:31 +0300

    Partial rebalance, enable unit building

--- scripts/unit.js
+++ scripts/unit.js
@@ -1,15 +1,14 @@
 const Drone = extend(UnitType, "Drone", {});
+const Harvester = extend(UnitType,  "Harvester", {});
 const Baneling = extend(UnitType,  "Baneling", {});
 //this is relic of the past now
 //read it if you want
 Drone.constructor = () => extend(UnitEntity, {});
 Drone.defaultController = () => extend(MinerAI, {});
 
+Harvester.constructor = () => extend(UnitEntity, {});
+Harvester.defaultController = () => extend(MinerAI, {});
+
 Baneling.constructor = () => extend(UnitEntity, {});
 Baneling.defaultController = () => extend(SuicideAI, {});
 
-
-module.exports = {
-  Drone: Drone,
-  Baneling: Baneling
-}
