904443ba61773c5522df727e032a5b8d6c63af0b
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2023-12-27 14:47:53 +0700

    balls

--- scripts/unit.js
+++ scripts/unit.js
@@ -1,6 +1,6 @@
 const Drone = extend(UnitType, "Drone", {});
 const Harvester = extend(UnitType,  "Harvester", {});
-const Baneling = extend(UnitType,  "Baneling", {});
+//const Baneling = extend(UnitType,  "Baneling", {});
 //this is relic of the past now
 //read it if you want
 Drone.constructor = () => extend(UnitEntity, {});
@@ -9,5 +9,5 @@ Drone.defaultController = () => extend(MinerAI, {});
 Harvester.constructor = () => extend(UnitEntity, {});
 Harvester.defaultController = () => extend(MinerAI, {});
 
-Baneling.constructor = () => extend(UnitEntity, {});
-Baneling.defaultController = () => extend(SuicideAI, {});
+//Baneling.constructor = () => extend(UnitEntity, {});
+//Baneling.defaultController = () => extend(SuicideAI, {});
