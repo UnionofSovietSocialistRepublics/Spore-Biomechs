9d886bd592f58419a9a30e4db6bd52babbabc943
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-02-15 22:01:00 +0700

    fog remov

--- scripts/main.js
+++ scripts/main.js
@@ -3,5 +3,6 @@ Events.on(EventType.ClientLoadEvent, () => {
 
 Vars.mods.locateMod("jp").meta.author="Walter hertwell white";//js test
 
-
+//Screw fog all of my homies HATE fog
+Events.run(EventType.Trigger.preDraw, () => {Vars.state.rules.fog = false; Vars.state.rules.staticFog = false; Vars.state.rules.limitMapArea = false })
 })
