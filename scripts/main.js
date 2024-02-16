8701dda3607cad5103a2ba3cb95c3744e90c1f95
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2024-02-16 14:04:04 +0700

    remove the testing

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,4 @@ Events.on(EventType.ClientLoadEvent, () => {
 
 
 Vars.mods.locateMod("jp").meta.author="Walter hertwell white";//js test
-
-//Screw fog all of my homies HATE fog
-Events.run(EventType.Trigger.preDraw, () => {Vars.state.rules.fog = false; Vars.state.rules.staticFog = false; Vars.state.rules.limitMapArea = false })
 })
