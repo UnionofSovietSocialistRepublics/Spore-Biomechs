ceffb9c8498c058d8be62d390e43d473b04361f6
Horizon <71920146+UnionofSovietSocialistRepublics@users.noreply.github.com>
2025-04-08 18:45:12 +0700

    Updatium

--- scripts/main.js
+++ scripts/main.js
@@ -2,28 +2,28 @@ Events.on(EventType.ClientLoadEvent, () => {
     var rand = (Math.floor(Math.random()*5));
     switch(rand){
         case 1:
-        Vars.mods.locateMod("sporepod").meta.author="Walter white";
-        Vars.mods.locateMod("sporepod").meta.subtitle="Now with extra racism";
+        Vars.mods.locateMod("jp").meta.author="Hydroabyss & Coffee molecules";
+        Vars.mods.locateMod("jp").meta.subtitle="Now with extra coffee";
         break;
         case 2:
-        Vars.mods.locateMod("sporepod").meta.author="Walter hertwell white";
-        Vars.mods.locateMod("sporepod").meta.subtitle="Charging up my racism";
+        Vars.mods.locateMod("jp").meta.author="Hydroabyss & Neoplasm Molecule";
+        Vars.mods.locateMod("jp").meta.subtitle="Red sun over serpulo";
         break;
         case 3:
-        Vars.mods.locateMod("sporepod").meta.author="Moonupper";
-        Vars.mods.locateMod("sporepod").meta.subtitle="Red sun is now over paradise";
+        Vars.mods.locateMod("jp").meta.author="ssybaordyH & seluceloM retaW";
+        Vars.mods.locateMod("jp").meta.subtitle="...gnorw si gnihtemoS ?gnorw gnihtemos sI";
         break;
         case 4:
-        Vars.mods.locateMod("sporepod").meta.author="Water molecules";
-        Vars.mods.locateMod("sporepod").meta.subtitle="Extra frosty editon";
+        Vars.mods.locateMod("jp").meta.author="Hydrogen & Oxygen";
+        Vars.mods.locateMod("jp").meta.subtitle="2 H2 + O2 = 2 H2O!";
         break;
         case 5:
-        Vars.mods.locateMod("sporepod").meta.author="Horizon";
-        Vars.mods.locateMod("sporepod").meta.subtitle="Billions must use java.";
+        Vars.mods.locateMod("jp").meta.author="Abyss & Duke";
+        Vars.mods.locateMod("jp").meta.subtitle="Json has fallen, billion must java.";
         break;
         default:
-        Vars.mods.locateMod("sporepod").meta.author="Frozen Water";
-        Vars.mods.locateMod("sporepod").meta.subtitle="Now with extra difficulty spike"; //frozen farland reference!111!111 (frozen farland difficulty spike is so unreal I cant even) Also commented out because too long
+        Vars.mods.locateMod("jp").meta.author="Hydroabyssal & Germanium";
+        Vars.mods.locateMod("jp").meta.subtitle="For the Deutschland!";
         break;
     }
 })
